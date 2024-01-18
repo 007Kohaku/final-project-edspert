42117c713e5a033946b86ab26f607f8bc0f24b24
007Kohaku <mr148980@gmail.com>
2024-01-18 21:36:40 +0700

    final project edspert

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#7F56D9",
+        screen: "#F9F5FF",
+        write: "#646E83",
+      },
+    },
+  },
+  plugins: [],
+};
