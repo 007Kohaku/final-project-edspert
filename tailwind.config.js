2eab3a2d28a2a5df509355e38efea5e2ce2ad45e
007Kohaku <mr148980@gmail.com>
2024-01-21 19:28:32 +0700

    fix dropdown menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: ["**/*.{html,js}"],
   theme: {
     extend: {
+      display: ["group-focus"],
       colors: {
         primary: "#7F56D9",
         screen: "#F9F5FF",
